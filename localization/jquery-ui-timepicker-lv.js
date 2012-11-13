f225d67eaec6d4129fe5d4728b6fceee3a4cd050
pasive <janis.janovskis@gmail.com>
2012-11-13 00:57:30 +0000

    Adding Latvian translation for the jQuery Timepicker Addon

---
+++ localization/jquery-ui-timepicker-lv.js
@@ -0,0 +1,20 @@
+/* Latvian translation for the jQuery Timepicker Addon */
+/* Written by Janis Janovskis */
+(function($) {
+	$.timepicker.regional['lv'] = {
+		timeOnlyTitle: 'Izvēlieties laiku',
+		timeText: 'Laiks',
+		hourText: 'Stundas',
+		minuteText: 'Minūtes',
+		secondText: 'Sekundes',
+		millisecText: 'Milisekundes',
+		timezoneText: 'Laika josla',
+		currentText: 'Pašlaik',
+		closeText: 'Aizvērt',
+		timeFormat: 'HH:mm',
+		amNames: ['priekšpusdiena','AM', 'A'],
+		pmNames: ['pēcpusdiena','PM', 'P'],
+		isRTL: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['lv']);
+})(jQuery);
